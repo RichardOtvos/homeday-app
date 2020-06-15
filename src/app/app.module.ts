d58a8a738920974d2a53b150e0affa2ab6e2c37c
Richard Otvos <richard.otvos@cloudera.com>
2020-06-15 22:30:27 +0200

    Adds form functionality to personal data component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -26,12 +27,13 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
 
     MatProgressBarModule,
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/application-form/personal-data-form/personal-data-form.component.ts
+++ src/app/application-form/personal-data-form/personal-data-form.component.ts
@@ -1,15 +1,16 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormDataService} from "../../services/form-data.service";
 
 @Component({
   selector: 'app-personal-data-form',
   templateUrl: './personal-data-form.component.html',
-  styleUrls: ['./personal-data-form.component.scss']
+  styleUrls: ['./../shared-form-styles.scss', './personal-data-form.component.scss']
 })
 export class PersonalDataFormComponent implements OnInit {
 
   @Output() onNext: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(public formsData: FormDataService) {}
 
   ngOnInit(): void {
   }
